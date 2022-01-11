9616a59190cd5688b5b058f00c723df40e7a6d84
Lucas Pape <lucas.pape@web.de>
2022-01-11 15:57:38 +0100

    Save using android MediaStore

--- android/app/src/main/java/de/aztube/aztube_app/MainActivity.java
+++ android/app/src/main/java/de/aztube/aztube_app/MainActivity.java
@@ -1,20 +1,25 @@
 package de.aztube.aztube_app;
 
-import android.content.ContextWrapper;
-import android.os.Environment;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
 
 import androidx.annotation.NonNull;
 
 import com.github.kiulian.downloader.YoutubeDownloader;
 import com.github.kiulian.downloader.downloader.YoutubeProgressCallback;
-import com.github.kiulian.downloader.downloader.request.RequestVideoFileDownload;
 import com.github.kiulian.downloader.downloader.request.RequestVideoInfo;
+import com.github.kiulian.downloader.downloader.request.RequestVideoStreamDownload;
 import com.github.kiulian.downloader.model.videos.VideoInfo;
 import com.github.kiulian.downloader.model.videos.formats.AudioFormat;
 import com.github.kiulian.downloader.model.videos.formats.Format;
 import com.github.kiulian.downloader.model.videos.formats.VideoWithAudioFormat;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 import io.flutter.embedding.android.FlutterActivity;
@@ -32,21 +37,17 @@ public class MainActivity extends FlutterActivity {
                 .setMethodCallHandler((call, result) -> {
                     switch (call.method) {
                         case "downloadVideo":
-                            new Async<File>().run(() -> {
+                            new Async<Void>().run(() -> {
                                 VideoInfo videoInfo = requestVideoInfo(call.argument("videoId"));
                                 Format format = getVideoFormat(videoInfo, call.argument("quality"));
 
-                                return downloadVideo(format);
-                            }, (data) -> {
-                                if (data != null) {
-                                    result.success(data.getAbsolutePath());
-                                }
+                                downloadVideo(format, videoInfo, call.argument("quality").equals("audio_only"));
+                                return null;
+                            }, (garbage) -> {
+                                result.success("ok");
 
                                 return null;
                             });
-                            break;
-                        case "getMediaDir":
-                            result.success(getMediaDir());
                             break;
                         case "getThumbnailUrl":
                             new Async<String>().run(() -> {
@@ -116,31 +117,82 @@ public class MainActivity extends FlutterActivity {
         return format;
     }
 
-    public File downloadVideo(Format format) {
+    public void downloadVideo(Format format, VideoInfo videoInfo, Boolean audioOnly) {
         YoutubeDownloader youtubeDownloader = new YoutubeDownloader();
 
-        RequestVideoFileDownload request = new RequestVideoFileDownload(format).saveTo(new File(getMediaDir())).overwriteIfExists(true).callback(new YoutubeProgressCallback<File>() {
-            @Override
-            public void onDownloading(int progress) {
-                System.out.println(progress);
+        String filename;
+
+        if(audioOnly){
+            filename = "video_" + System.currentTimeMillis() + ".mp4";
+        }else{
+            filename = "audio_" + System.currentTimeMillis() + ".weba";
+        }
+
+        ContentValues contentValues = new ContentValues();
+        ContentResolver contentResolver = getContentResolver();
+
+        Uri uriSaved;
+
+        if(audioOnly){
+            contentValues.put(MediaStore.Audio.Media.TITLE, filename);
+            contentValues.put(MediaStore.Audio.Media.DISPLAY_NAME, videoInfo.details().title());
+            contentValues.put(MediaStore.Audio.Media.MIME_TYPE, "audio/ogg");
+            contentValues.put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/" + "AZTube");
+            contentValues.put(MediaStore.Audio.Media.DATE_ADDED, System.currentTimeMillis()/1000);
+            contentValues.put(MediaStore.Audio.Media.DATE_TAKEN, System.currentTimeMillis());
+            contentValues.put(MediaStore.Audio.Media.IS_PENDING, 1);
+
+            Uri collection = MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+            uriSaved = contentResolver.insert(collection, contentValues);
+        }else{
+            contentValues.put(MediaStore.Video.Media.TITLE, filename);
+            contentValues.put(MediaStore.Video.Media.DISPLAY_NAME, videoInfo.details().title());
+            contentValues.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
+            contentValues.put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/" + "AZTube");
+            contentValues.put(MediaStore.Video.Media.DATE_ADDED, System.currentTimeMillis()/1000);
+            contentValues.put(MediaStore.Video.Media.DATE_TAKEN, System.currentTimeMillis());
+            contentValues.put(MediaStore.Video.Media.IS_PENDING, 1);
+
+            Uri collection = MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+            uriSaved = contentResolver.insert(collection, contentValues);
+        }
+
+        ParcelFileDescriptor parcelFileDescriptor;
+
+        try {
+            parcelFileDescriptor = getContentResolver().openFileDescriptor(uriSaved, "w");
+
+            FileOutputStream fileOutputStream = new FileOutputStream(parcelFileDescriptor.getFileDescriptor());
+
+            RequestVideoStreamDownload request = new RequestVideoStreamDownload(format, fileOutputStream).callback(new YoutubeProgressCallback<Void>() {
+                @Override
+                public void onDownloading(int progress) {
+                    System.out.println(progress);
+                }
+
+                @Override
+                public void onFinished(Void data) {
+
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    System.out.println("Error downloading video");
+                }
+            });
+
+            youtubeDownloader.downloadVideoStream(request).data();
+
+            contentValues.clear();
+            if(audioOnly){
+                contentValues.put(MediaStore.Audio.Media.IS_PENDING, 0);
+            }else{
+                contentValues.put(MediaStore.Video.Media.IS_PENDING, 0);
             }
 
-            @Override
-            public void onFinished(File data) {
-                System.out.println("Finished!");
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("Error downloading video");
-            }
-        });
-
-        return youtubeDownloader.downloadVideoFile(request).data();
-    }
-
-    public String getMediaDir() {
-        ContextWrapper contextWrapper = new ContextWrapper(this);
-        return contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MOVIES).toString();
+            getContentResolver().update(uriSaved, contentValues, null, null);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
