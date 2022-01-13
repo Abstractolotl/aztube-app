64a102934628b1eecf1fdd11adaba188320a9ec5
dseydel <dseydel@united-internet.de>
2022-01-13 15:57:51 +0100

    Display Download Type on Dashboard

--- lib/elements/download.dart
+++ lib/elements/download.dart
@@ -48,9 +48,10 @@ class DownloadState extends State<Download> {
     if(downloading){
       trailing = CircularProgressIndicator(color: Colors.green, value: widget.video.progress/100,);
     }
+    var type = widget.video.quality == 'audio' ? ' [Audio]' : ' [Video]';
     return Column(children: [
       ListTile(
-          title: Text(widget.video.title),
+          title: Text(widget.video.title + type),
           trailing: trailing,
           onLongPress: openInformationView,
       ),
