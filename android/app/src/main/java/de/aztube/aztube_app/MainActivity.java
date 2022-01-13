d197a20f24cdc2261d45c7e139a4366554c0e74d
Lucas Pape <lucas.pape@web.de>
2022-01-13 10:49:51 +0100

    Move download methods to download class

--- android/app/src/main/java/de/aztube/aztube_app/MainActivity.java
+++ android/app/src/main/java/de/aztube/aztube_app/MainActivity.java
@@ -1,31 +1,10 @@
 package de.aztube.aztube_app;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
-
 import androidx.annotation.NonNull;
-
 import androidx.annotation.Nullable;
-import com.github.kiulian.downloader.YoutubeDownloader;
-import com.github.kiulian.downloader.downloader.YoutubeProgressCallback;
-import com.github.kiulian.downloader.downloader.request.RequestVideoInfo;
-import com.github.kiulian.downloader.downloader.request.RequestVideoStreamDownload;
-import com.github.kiulian.downloader.model.videos.VideoInfo;
-import com.github.kiulian.downloader.model.videos.formats.AudioFormat;
-import com.github.kiulian.downloader.model.videos.formats.Format;
-import com.github.kiulian.downloader.model.videos.formats.VideoWithAudioFormat;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Objects;
 
 import io.flutter.embedding.android.FlutterActivity;
@@ -58,28 +37,25 @@ public class MainActivity extends FlutterActivity {
                                 String quality = call.argument("quality");
                                 String downloadId = call.argument("downloadId");
 
-                                VideoInfo videoInfo = requestVideoInfo(videoId);
+                                return Downloader.downloadVideo(this, videoId, downloadId, quality, (videoId1, downloadId1, progress) -> {
+                                    HashMap<String, Object> args = new HashMap<>();
 
-                                Format format = null;
-                                if (quality != null) {
-                                    format = getVideoFormat(videoInfo, quality);
-                                }
+                                    args.put("videoId", videoId1);
+                                    args.put("downloadId", downloadId1);
+                                    args.put("progress", progress);
 
-                                if(format != null){
-                                    return downloadVideo(format, videoInfo, videoId, downloadId, quality.equals("audio_only"));
-                                }else{
-                                    return false;
-                                }
+                                    new Async<Void>().run(() -> null, (garbage) -> {
+                                        channel.invokeMethod("progress", args);
+                                        return null;
+                                    });
+                                });
                             }, (success) -> {
                                 result.success(success);
                                 return null;
                             });
                             break;
                         case "getThumbnailUrl":
-                            new Async<String>().run(() -> {
-                                VideoInfo videoInfo = requestVideoInfo(call.argument("videoId"));
-                                return getThumbnailUrl(videoInfo);
-                            }, (String data) -> {
+                            new Async<String>().run(() -> Downloader.getThumbnailUrl(call.argument("videoId")), (String data) -> {
                                 if (data != null) {
                                     result.success(data);
                                 }else{
@@ -101,150 +77,5 @@ public class MainActivity extends FlutterActivity {
                             break;
                     }
                 });
-    }
-
-    public VideoInfo requestVideoInfo(String videoId) {
-        YoutubeDownloader youtubeDownloader = new YoutubeDownloader();
-
-        RequestVideoInfo request = new RequestVideoInfo(videoId);
-
-        return youtubeDownloader.getVideoInfo(request).data();
-    }
-
-    public String getThumbnailUrl(VideoInfo videoInfo) {
-        List<String> thumbnails = videoInfo.details().thumbnails();
-
-        return thumbnails.get(thumbnails.size()-1);
-    }
-
-    public Format getVideoFormat(VideoInfo videoInfo, String quality) {
-        List<VideoWithAudioFormat> videoFormats = videoInfo.videoWithAudioFormats();
-        List<AudioFormat> audioFormats = videoInfo.audioFormats();
-
-        Format format = null;
-
-        if (quality.equals("audio_only")) {
-            if (audioFormats.size() > 0) {
-                format = audioFormats.get(0);
-
-                for (AudioFormat audioFormat : audioFormats) {
-                    if (audioFormat.averageBitrate() > ((AudioFormat) format).averageBitrate()) {
-                        format = audioFormat;
-                    }
-                }
-            } else {
-                System.out.println("No audio formats available!");
-                return null;
-            }
-        } else {
-            for (VideoWithAudioFormat videoFormat : videoFormats) {
-                if (videoFormat.qualityLabel().equals(quality)) {
-                    format = videoFormat;
-                    break;
-                }
-            }
-
-            if (format == null) {
-                System.out.println("Could not find video format!");
-                return null;
-            }
-        }
-
-        return format;
-    }
-
-    public boolean downloadVideo(Format format, VideoInfo videoInfo, String videoId, String downloadId, Boolean audio) {
-        final Boolean[] success = {false};
-
-        YoutubeDownloader youtubeDownloader = new YoutubeDownloader();
-
-        String filename;
-
-        if(audio){
-            filename = "video_" + downloadId + ".mp4";
-        }else{
-            filename = "audio_" + downloadId + ".weba";
-        }
-
-        ContentValues contentValues = new ContentValues();
-        ContentResolver contentResolver = getContentResolver();
-
-        Uri uriSaved;
-
-        if(audio){
-            contentValues.put(MediaStore.Audio.Media.TITLE, filename);
-            contentValues.put(MediaStore.Audio.Media.DISPLAY_NAME, videoInfo.details().title());
-            contentValues.put(MediaStore.Audio.Media.MIME_TYPE, "audio/ogg");
-            contentValues.put(MediaStore.Audio.Media.RELATIVE_PATH, "Music/" + "AZTube");
-            contentValues.put(MediaStore.Audio.Media.DATE_ADDED, System.currentTimeMillis()/1000);
-            contentValues.put(MediaStore.Audio.Media.DATE_TAKEN, System.currentTimeMillis());
-            contentValues.put(MediaStore.Audio.Media.IS_PENDING, 1);
-
-            Uri collection = MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
-            uriSaved = contentResolver.insert(collection, contentValues);
-        }else{
-            contentValues.put(MediaStore.Video.Media.TITLE, filename);
-            contentValues.put(MediaStore.Video.Media.DISPLAY_NAME, videoInfo.details().title());
-            contentValues.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
-            contentValues.put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/" + "AZTube");
-            contentValues.put(MediaStore.Video.Media.DATE_ADDED, System.currentTimeMillis()/1000);
-            contentValues.put(MediaStore.Video.Media.DATE_TAKEN, System.currentTimeMillis());
-            contentValues.put(MediaStore.Video.Media.IS_PENDING, 1);
-
-            Uri collection = MediaStore.Video.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY);
-            uriSaved = contentResolver.insert(collection, contentValues);
-        }
-
-        ParcelFileDescriptor parcelFileDescriptor;
-
-        try {
-            parcelFileDescriptor = getContentResolver().openFileDescriptor(uriSaved, "w");
-
-            FileOutputStream fileOutputStream = new FileOutputStream(parcelFileDescriptor.getFileDescriptor());
-
-            RequestVideoStreamDownload request = new RequestVideoStreamDownload(format, fileOutputStream).callback(new YoutubeProgressCallback<Void>() {
-                @Override
-                public void onDownloading(int progress) {
-                    HashMap<String, Object> args = new HashMap<>();
-                    args.put("progress", progress);
-                    args.put("videoId", videoId);
-                    args.put("downloadId", downloadId);
-
-                    new Async<Void>().run(() -> null, (garbage) -> {
-                        channel.invokeMethod("progress", args);
-                        return null;
-                    });
-                }
-
-                @Override
-                public void onFinished(Void data) {
-                    success[0] = true;
-                }
-
-                @Override
-                public void onError(Throwable throwable) {
-                    System.out.println("Error downloading video");
-                    success[0] = false;
-                }
-            });
-
-            youtubeDownloader.downloadVideoStream(request).data();
-
-            if(success[0]){
-                contentValues.clear();
-
-                if(audio){
-                    contentValues.put(MediaStore.Audio.Media.IS_PENDING, 0);
-                }else{
-                    contentValues.put(MediaStore.Video.Media.IS_PENDING, 0);
-                }
-
-                getContentResolver().update(uriSaved, contentValues, null, null);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return success[0];
     }
 }
