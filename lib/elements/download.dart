3aa5073134f9b758360958e124a55fd19e123587
Kevin Fischer <KevinFischer2010@gmail.com>
2022-01-13 21:28:19 +0100

    better ui downloads

--- lib/elements/download.dart
+++ lib/elements/download.dart
@@ -7,8 +7,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Download extends StatefulWidget {
-
-  const Download({Key? key, required this.video, required this.cache, required this.state}) : super(key: key);
+  const Download(
+      {Key? key, required this.video, required this.cache, required this.state})
+      : super(key: key);
 
   final DashboardScreenState state;
   final DownloadCache cache;
@@ -18,16 +19,14 @@ class Download extends StatefulWidget {
   State<StatefulWidget> createState() {
     return DownloadState();
   }
-
 }
 
 class DownloadState extends State<Download> {
-
   bool downloading = false;
 
   @override
   void initState() {
-    if(widget.video.progress > 0 && !widget.video.downloaded){
+    if (widget.video.progress > 0 && !widget.video.downloaded) {
       downloading = true;
     }
     super.initState();
@@ -38,10 +37,11 @@ class DownloadState extends State<Download> {
     Widget trailing = IconButton(
       enableFeedback: !(!widget.video.downloaded && !downloading),
       onPressed: () {
-        if(!widget.video.downloaded && !downloading){
+        if (!widget.video.downloaded && !downloading) {
           startDownload();
         }
       },
+<<<<<<< Updated upstream
       icon: Icon(widget.video.downloaded  ? Icons.download_done : Icons.download),
       color: Colors.black,
     );
@@ -53,19 +53,78 @@ class DownloadState extends State<Download> {
           title: Text(widget.video.title),
           trailing: trailing,
           onLongPress: openInformationView,
+=======
+      icon:
+          Icon(widget.video.downloaded ? Icons.download_done : Icons.download),
+      color: widget.video.downloaded ? Colors.green : Colors.black,
+    );
+    if (downloading) {
+      //https://i.imgur.com/Z51QssF.png
+      trailing = CircularProgressIndicator(
+        color: Colors.green,
+        value: widget.video.progress / 100,
+      );
+    }
+    return Column(children: [
+      ListTile(
+        contentPadding: const EdgeInsets.all(0),
+        visualDensity: VisualDensity(horizontal: 0, vertical: 0),
+        dense: true,
+        horizontalTitleGap: 5,
+        leading: Image.network(
+            "https://i.ytimg.com/vi/xyVfLxV08I0/maxresdefault.jpg"),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              children: [
+                Text(
+                  widget.video.title + "sehr sehr langer langer titel itel",
+                  style: TextStyle(fontSize: 17),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.video.author,
+                      style: TextStyle(
+                          fontSize: 10.0,
+                          color: Colors.black.withOpacity(0.25)),
+                    ),
+                    Text(
+                      widget.video.downloadId.toString() +
+                          " - " +
+                          widget.video.videoId +
+                          " - " +
+                          widget.video.quality,
+                      style: TextStyle(
+                          fontSize: 10.0,
+                          color: Colors.black.withOpacity(0.25)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+        trailing: trailing,
+        onLongPress: openInformationView,
+>>>>>>> Stashed changes
       ),
       const Divider()
     ]);
   }
 
-  void openInformationView(){
+  void openInformationView() {
     Route route = MaterialPageRoute(
-        builder: (context) => DownloadScreen(video: widget.video, cache: widget.cache));
+        builder: (context) =>
+            DownloadScreen(video: widget.video, cache: widget.cache));
     Navigator.push(context, route).then(widget.state.reload);
   }
 
-  void startDownload(){
-    if(!downloading && !widget.video.downloaded){
+  void startDownload() {
+    if (!downloading && !widget.video.downloaded) {
       setState(() {
         downloading = true;
       });
@@ -82,18 +141,19 @@ class DownloadState extends State<Download> {
     };
 
     final dynamic result = await platform.invokeMethod("downloadVideo", args);
-    try{
-      if(!result){
+    try {
+      if (!result) {
         setState(() {
           downloading = false;
         });
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Download failed'),
+          const SnackBar(
+              content: Text('Download failed'),
               duration: Duration(seconds: 2),
               behavior: SnackBarBehavior.floating),
         );
       }
-    }catch(e){
+    } catch (e) {
       widget.cache.queue.remove(widget.video);
 
       widget.video.downloaded = true;
@@ -106,5 +166,4 @@ class DownloadState extends State<Download> {
       });
     }
   }
-
 }
