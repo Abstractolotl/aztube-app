2ea9dfba8a9f6a79d6d227fd044a28bce1aa3eb2
dseydel <dseydel@united-internet.de>
2022-01-13 10:54:16 +0100

    Implement channel messages for file changes

--- lib/files/filemanager.dart
+++ lib/files/filemanager.dart
@@ -4,9 +4,12 @@ import 'dart:io';
 import 'package:aztube/files/downloadsmodel.dart';
 import 'package:aztube/files/i_filemanager.dart';
 import 'package:aztube/files/settingsmodel.dart';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileManager extends IFileManager{
+
+  static const platform = MethodChannel("de.aztube.aztube_app/youtube");
 
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
@@ -36,6 +39,7 @@ class FileManager extends IFileManager{
   void saveSettings(Settings settings) async {
     final file = await _settingsFile;
     file.writeAsStringSync(jsonEncode(settings.toJson()));
+    platform.invokeMethod('settingsChanged');
   }
 
 
@@ -52,5 +56,6 @@ class FileManager extends IFileManager{
   void saveDownloads(DownloadCache downloads) async {
     final file = await _downloadsFile;
     file.writeAsStringSync(jsonEncode(downloads.toJson()));
+    platform.invokeMethod('downloadsChanged');
   }
 }
