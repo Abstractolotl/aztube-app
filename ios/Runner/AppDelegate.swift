be465389ff3880c9c4e972da1e65d3d358f51019
Lucas Pape <admin@lucaspape.de>
2022-01-16 00:07:06 +0100

    Fix the objective c garbage

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Runner
+//
+//  Created by Lucas Pape on 15.01.22.
+//
+
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
