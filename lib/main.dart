7ed2fea81c37fa6c46aaf97009e7af2695beede8
Ancocodet <ancoplays@gmail.com>
2022-07-22 11:28:17 +0200

    Update button theme

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class AzTube extends StatelessWidget {
         textTheme: const TextTheme(
           headline1: TextStyle(fontSize: 17, color: Colors.white),
           bodyText1: TextStyle(fontSize: 10.0, color: Colors.white54),
-          button: TextStyle(fontSize: 12.0, color: Colors.white54)
+          buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary)
         )
     );
   }
@@ -53,7 +53,7 @@ class AzTube extends StatelessWidget {
         textTheme: const TextTheme(
           headline1: TextStyle(fontSize: 17, color: Colors.black),
           bodyText1: TextStyle(fontSize: 10.0, color: Colors.black38),
-          button: TextStyle(fontSize: 12.0, color: Colors.black38)
+          buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.primary)
         )
     );
   }
