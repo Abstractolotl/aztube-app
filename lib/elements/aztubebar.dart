7c2c0c5b85ca2a210adbcabfe76e4a2f43a556a4
Kevin Fischer <kfischer@united-internet.de>
2023-07-07 21:44:31 +0200

    Remove ANDROID

--- lib/elements/aztubebar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:aztube/files/settingsmodel.dart';
-import 'package:aztube/views/settings.dart';
-import 'package:flutter/material.dart';
-
-class AzTubeBar{
-
-  static const Text title = Text('AzTube');
-  
-}
